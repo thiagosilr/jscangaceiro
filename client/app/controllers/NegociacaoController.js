f71d790272f5f8035e3eea334d27fa605298e143
Thiago Silva Resende <thiago.resende@ivoryit.local>
2017-12-12 13:04:49 -0200

    3.3 Associando métodos do controller às ações do usuário

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,17 @@
 class NegociacaoController {
 	Adicionar(event) {
 		event.preventDefault();
+
+		// Obtem os dados do formulário
+		let $ = document.querySelector.bind(document);
+		let campoData = $('#data');
+		let campoQuantidade = $('#quantidade');
+		let campoValor = $('#valor');
+
+		console.log(campoData.value);
+		console.log(parseInt(campoQuantidade.value));
+		console.log(parseFloat(campoValor.value));
+
 		console.log('Ação executada');
 	}
 }
